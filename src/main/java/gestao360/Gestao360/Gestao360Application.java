496e3d9830bd051ca327667d7998e3e53ac68242
wenderson jonatha <wendersonkw1@gmail.com>
2025-05-26 18:38:10 -0300

    Inicio da jornada

---
+++ src/main/java/gestao360/Gestao360/Gestao360Application.java
@@ -0,0 +1,15 @@
+package gestao360.Gestao360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gestao360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gestao360Application.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/gestao360/Gestao360/Gestao360ApplicationTests.java
@@ -0,0 +1,13 @@
+package gestao360.Gestao360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gestao360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
