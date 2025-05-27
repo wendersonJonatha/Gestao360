b40f0c4319319dd945f06054f08c59b0154f133e
wenderson jonatha <wendersonkw1@gmail.com>
2025-05-27 13:15:27 -0300

    Criar classe Tarefa no pacote model

---
+++ src/main/java/gestao360/Gestao360/model/Tarefa.java
@@ -0,0 +1,32 @@
+package gestao360.Gestao360.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTarefa;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private LocalDate prazo;
+
+    @Column(nullable = false)
+    private boolean finalizada;
+}
