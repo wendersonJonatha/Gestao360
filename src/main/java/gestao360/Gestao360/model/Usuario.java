94748cb694f4148575bde3e43e3b1038c5ba19be
wenderson jonatha <wendersonkw1@gmail.com>
2025-05-26 23:01:07 -0300

    Criação da Class Usuario na pasta Model

---
+++ src/main/java/gestao360/Gestao360/model/Usuario.java
@@ -0,0 +1,31 @@
+package gestao360.Gestao360.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String cargo;
+}
