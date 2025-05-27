fd25677db1642ce002d1df7234dd57b71752858f
wenderson jonatha <wendersonkw1@gmail.com>
2025-05-27 13:41:18 -0300

    Criar repositório UsuarioRepository com JpaRepository

---
+++ src/main/java/gestao360/Gestao360/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package gestao360.Gestao360.repository;
+
+import gestao360.Gestao360.model.Unidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Unidade, Long> {
+}
